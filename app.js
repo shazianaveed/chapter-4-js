faba585e2fecf1b076a938bd38d547d5cda783a6
shazianaveed <160173580+shazianaveed@users.noreply.github.com>
2025-01-18 22:55:21 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+
+var age = 15;
+alert("i am " + age + " years old")
+
+// Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+
+var visited = 14;
+alert(" you have visited this site " + visited + " times");
+
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+
+var age = 1990;
+alert (" my birth year is " +  age);
